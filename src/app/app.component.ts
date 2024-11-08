c339cdc69fd6fd6b064f45fd06d33ac888d4cc9b
Kyle Wheeless <kyle@wheeless.dev>
2024-11-08 11:36:32 -0800

    updates to portfolio and initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { NgOptimizedImage } from '@angular/common';
+
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, NgOptimizedImage],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'AngularKyleWheelessPortfolio2024';
+    public readonly me =
+        'https://imagedelivery.net/eFpPUO445KI7hjnFeTxrJQ/10228445-077e-41f4-a888-00d41a437600/public';
+    public readonly title = 'Kyle W. Wheeless';
 }
