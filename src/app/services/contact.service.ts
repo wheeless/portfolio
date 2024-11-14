606b5bfa573655c428e58bd0e1bd067836130c3f
Kyle Wheeless <kyle@wheeless.dev>
2024-11-14 10:20:08 -0800

    update handling of token

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -19,12 +19,24 @@ interface ContactResponse {
 })
 export class ContactService {
     private readonly apiUrl = 'https://api.avernix.com/api/v2/mail/form?mail_token=';
-    private readonly mail_token = 'Tyq7-YXMiADdPpZa9ig1';
+    private readonly tokens = {
+        'wheeless.dev': 'Tyq7-YXMiADdPpZa9ig1',
+        'kylewheeless.com': '4YiCI8wiodsAAMtq--sg',
+    };
+
     constructor(private http: HttpClient) {}
 
+    private getMailToken(): string {
+        const hostname = window.location.hostname;
+        // Remove 'www.' if present
+        const domain = hostname.replace('www.', '');
+
+        // Return the appropriate token or default to wheeless.dev token
+        return this.tokens[domain as keyof typeof this.tokens] || this.tokens['kylewheeless.com'];
+    }
+
     public async submitContact(formData: ContactForm): Promise<ContactResponse> {
-        return firstValueFrom(
-            this.http.post<ContactResponse>(this.apiUrl + this.mail_token, formData),
-        );
+        const token = this.getMailToken();
+        return firstValueFrom(this.http.post<ContactResponse>(this.apiUrl + token, formData));
     }
 }
