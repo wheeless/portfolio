a07c4db5d0816e5ff479a98bd0a7022d42ba4da9
Kyle Wheeless <kyle@wheeless.dev>
2024-12-21 15:30:40 -0800

    random console log updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,6 @@ import { RouterOutlet } from '@angular/router';
     styleUrl: './app.component.css',
 })
 export class AppComponent {
-    public readonly me =
-        'https://imagedelivery.net/eFpPUO445KI7hjnFeTxrJQ/a1d3c2a4-e8e7-4697-cead-365630bc7e00/public';
-    public readonly title = 'Kyle W. Wheeless';
-    public readonly avernix =
-        'https://imagedelivery.net/eFpPUO445KI7hjnFeTxrJQ/f8a1973b-bd9b-4259-3e29-c4116bcc0200/public';
-
     ngOnInit() {
         console.group('Whatcha doin?');
         console.log(
